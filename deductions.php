cc176b1f5216786f3286493889be24271c0a237c
Chakasisgood <matthewsolar06@gmail.com>
2023-12-07 13:30:02 +0800

    semicolon

--- deductions.php
+++ deductions.php
@@ -109,7 +109,7 @@
 	}
 	img{
 		max-width:100px;
-		max-height: :150px;
+		max-height: 150px;
 	}
 </style>
 <script>
--- department.php
+++ department.php
@@ -103,7 +103,7 @@
 	}
 	img{
 		max-width:100px;
-		max-height: :150px;
+		max-height: 150px;
 	}
 </style>
 <script>
